7a0c33d2a1ec51d8bb749c0adc1c03648b51a64e
sofia va <sophia.lily.lee@gmail.com>
2024-08-26 22:20:27 +0300

    Holiday Service created

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,10 @@
 package ru.netology;
 
+import ru.netology.learnPackagas.services.HolidayService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HolidayService service = new HolidayService();
+        System.out.println(service.calculate(10_000, 3_000, 20_000));
     }
 }
