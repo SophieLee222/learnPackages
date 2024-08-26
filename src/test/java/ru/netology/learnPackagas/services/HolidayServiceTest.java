12af2271deaff67797c1194d52350fa5602dee4c
sofia va <sophia.lily.lee@gmail.com>
2024-08-26 22:48:27 +0300

    Added Parameterized Test

--- src/test/java/ru/netology/learnPackagas/services/HolidayServiceTest.java
+++ src/test/java/ru/netology/learnPackagas/services/HolidayServiceTest.java
@@ -2,18 +2,16 @@ package ru.netology.learnPackagas.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class HolidayServiceTest {
 
-    @Test
-    void shouldCalculateForIncome10000Expenses3000Threshold20000() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/holiday.csv")
+    public void test(int expected, int income, int expenses, int threshold) {
         HolidayService service = new HolidayService();
-
-        // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
@@ -22,20 +20,4 @@ public class HolidayServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateForIncome100000Expenses60000Threshold150000() {
-        HolidayService service = new HolidayService();
-
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
 }
